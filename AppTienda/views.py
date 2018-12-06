06ddef885e1ac148a377f8b1b98940af446c1589
Katherine Alvarez <kalvarez@nisum.com>
2018-12-06 00:44:28 -0300

    corrigiendo formulario y agregando api dolar

--- AppTienda/views.py
+++ AppTienda/views.py
@@ -1,16 +1,27 @@
 from django.http import HttpResponse
 import requests
 from django.shortcuts import render
+from django.shortcuts import redirect
 from  . import models
 from .forms import VentaForm,LoginForm
 
 def index(request):
-    return render(request,'index.html')
+    response = requests.get('https://mindicador.cl/api')
+    data = response.json()
+    return render(request,'index.html',{'dolar': data['dolar']['valor']})
+
 
 def login(request):
     return render(request,'login.html')
 
 def registroVenta(request):
     form = VentaForm()
-    form.save()
+    if request.method == "POST":
+        form = VentaForm(request.POST)
+        if form.is_valid():
+            venta = form.save(commit=False)
+            venta.save()
+            return redirect('index')
+    else:
+        form = VentaForm()
     return render(request, 'registroVenta.html', {'form': form})
